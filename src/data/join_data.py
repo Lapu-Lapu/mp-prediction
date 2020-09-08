7924b587356d9315f20720025dff41a1c5083da4
Benjamin Knopp <benjamin.knopp@uni-marburg.de>
2020-09-08 09:19:18 +0200

    Add join_data.

---
+++ src/data/join_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df_vr = pd.read_json('data/processed/processed_data_vr.json')
+print('vr data:', df_vr.shape)
+
+df_online = pd.read_json('data/processed/processed_data_online.json')
+print('online data:', df_online.shape)
+df_online['participant'] = df_online['sona']
+
+df = pd.concat([df_vr, df_online], join='inner', ignore_index=True)
+print('data:', df.shape)
+
+df.to_json('data/processed/processed_data.json')
