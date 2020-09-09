1f5e50df8d429e7d2583007528e5710a506c89fb
Benjamin Knopp <benjamin.knopp@uni-marburg.de>
2020-09-09 09:22:57 +0200

    Add variance priors.

--- src/models/two_level.py
+++ src/models/two_level.py
@@ -9,10 +9,12 @@ df, pmps, participants, mp_types, participant_to_idx, mp_type_to_idx, pmp_to_idx
 
 with pm.Model() as multilevel:
     a_barbar = pm.Normal('a_barbar', 0, 1.5)
-    a_bar = pm.Normal('a_bar', a_barbar, 1.5, shape=len(mp_types))
+    sigma_abar = pm.Exponential('sigma_abar', 1)
+    sigma_bbar = pm.Exponential('sigma_bbar', 1)
+    a_bar = pm.Normal('a_bar', a_barbar, sigma_abar, shape=len(mp_types))
     sigma_a = pm.Exponential('sigma_a', 1, shape=len(mp_types))
     b_barbar = pm.Normal('b_barbar', 0, 1.5)
-    b_bar = pm.Normal('b_bar', b_barbar, 1.5, shape=len(mp_types))
+    b_bar = pm.Normal('b_bar', b_barbar, sigma_bbar, shape=len(mp_types))
     sigma_b = pm.Exponential('sigma_b', 1, shape=len(mp_types))
     # c_bar = pm.Normal('c_bar', 0, 1.5)
     # sigma_c = pm.Exponential('sigma_c', 1)
@@ -26,7 +28,7 @@ with pm.Model() as multilevel:
     p = c[df.id_mptype] / (1 + np.exp(-(a_bar[df.id_mptype] + sigma_a[df.id_mptype] * a[df.id_participant] + (b_bar[df.id_mptype] + sigma_b[df.id_mptype] * b[df.id_participant]) * x)))
     # p = 0.5 / (1 + np.exp(-(a_bar + sigma_a* a[df.id_participant] + sigma_c*c[df.id_mptype] + (b_bar + sigma_b * b[df.id_mptype]) * x)))
     s = pm.Bernoulli('s', p=p, observed=df['result'])
-    trace_multilevel = pm.sample(1000, tune=1000, init='adapt_diag')
+    trace_multilevel = pm.sample(2000, tune=1000, init='adapt_diag')
 
 with open("data/processed/twolevel_trace.pkl", "wb") as fh:
     pickle.dump((multilevel, trace_multilevel), fh)
