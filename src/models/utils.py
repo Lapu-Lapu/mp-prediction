45e2a138c1fb01e74ec58d9e73d20afffc8da6ff
Benjamin Knopp <knoppbe@staff.uni-marburg.de>
2020-09-10 11:32:00 +0200

    Add index adder.

--- src/models/utils.py
+++ src/models/utils.py
@@ -17,19 +17,20 @@ def key_to_idx(keys):
     return dict(zip(keys, range(len(keys))))
 
 
+def add_index(df, key):
+    unique = df[key].unique()
+    mapping = key_to_idx(unique)
+    df[f'id_{key}'] = df[key].apply(lambda x: mapping[x])
+    return unique, mapping, df
+
+
 def load_data(fn='data/processed/processed_data.json'):
     df = pd.read_json('data/processed/processed_data_online.json')
 
     df['participant'] = df.participant.apply(str)
     df['pmp'] = df.apply(lambda row: ';'.join([row.participant, row.mp_type]), axis=1)
-    pmps = df.pmp.unique()
-    participants = df.participant.unique()
-    mp_types = df.mp_type.unique()
-    participant_to_idx = key_to_idx(participants)
-    mp_type_to_idx = key_to_idx(mp_types)
-    pmp_to_idx = key_to_idx(pmps)
 
-    df['id_mptype'] = df.mp_type.apply(lambda x: mp_type_to_idx[x])
-    df['id_participant'] = df.participant.apply(lambda x: participant_to_idx[x])
-    df['id_pmp'] = df.pmp.apply(lambda x: pmp_to_idx[x])
+    participants, participant_to_idx, df = add_index(df, 'participant')
+    mp_types, mp_type_to_idx, df = add_index(df, 'mp_type')
+    pmps, pmp_to_idx, df = add_index(df, 'pmp')
     return df, pmps, participants, mp_types, participant_to_idx, mp_type_to_idx, pmp_to_idx
