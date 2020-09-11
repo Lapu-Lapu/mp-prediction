ce1afaef1bd747bbf39ce32d3053dc249256bf39
Benjamin Knopp <benjamin.knopp@uni-marburg.de>
2020-09-11 13:53:15 +0200

    Remove fixed pelvis trials.

--- src/data/join_data.py
+++ src/data/join_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df_vr = pd.read_json('data/processed/processed_data_vr.json')
 df_vr['setting'] = 'vr'
@@ -6,6 +7,18 @@ df_vr['occluded_contact'] = df_vr.apply(
         lambda row:
         (row.movement in ['pass-botte', 'return-bottle']),
         axis=1)
+print('vr data:', df_vr.shape)
+df_vr['fn'] = df_vr.apply(lambda row:
+          f'model({row.mp_type})-dataset({row.movement})-npsi({int(row.npsi)})-hold({row.hold})' if ~np.isnan(row.npsi) else '',
+                          axis=1)
+
+ghost_experiment = pd.read_csv(
+    'data/raw/online/attention_info/test_dmp.csv')
+attcheck_set = set(ghost_experiment[ghost_experiment.ghosting].fn)
+attcheck_idx = df_vr.fn.apply(lambda s: s in attcheck_set)
+df_vr['attention_check'] = attcheck_idx
+df_vr = df_vr[~df_vr['attention_check']]
+# df_vr.drop(columns=['fn'], inplace=True)
 print('vr data:', df_vr.shape)
 
 df_online = pd.read_json('data/processed/processed_data_online.json')
