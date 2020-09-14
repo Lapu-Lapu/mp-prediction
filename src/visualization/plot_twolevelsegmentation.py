7ff7d5c803673109c40a5bb5f2fb0d2bdafb0d42
Benjamin Knopp <benjamin.knopp@uni-marburg.de>
2020-09-14 12:23:10 +0200

    Use violinplot and use restrict movement types.

--- src/visualization/plot_twolevelsegmentation.py
+++ src/visualization/plot_twolevelsegmentation.py
@@ -8,7 +8,7 @@ from src.models.utils import beta_std, center_data
 from src.models.utils import load_data
 
 
-df, pmps, participants, mp_types, participant_to_idx, mp_type_to_idx, pmp_to_idx = load_data('data/processed/processed_online_data.json')
+df, pmps, participants, mp_types, participant_to_idx, mp_type_to_idx, pmp_to_idx = load_data('data/processed/processed_data_online.json')
 
 # occ == 1 if contact occured during occlusion
 df['occ'] = df['occluded_contact'].astype(int)
@@ -17,7 +17,7 @@ mp_to_indices = {mp: [idx for s, idx in pmp_to_idx.items() if s.split(';')[1] ==
 
 with open("data/processed/twolevelsegmentation_trace.pkl", "rb") as fh:
     model, trace = pickle.load(fh)
-mp_type_to_idx = {'vcgpdm': 0, 'vgpdm': 1, 'tmp': 2, 'dmp': 3, 'mapgpdm': 4, 'mapcgpdm': 5}
+# mp_type_to_idx = {'vcgpdm': 0, 'vgpdm': 1, 'tmp': 2, 'dmp': 3, 'mapgpdm': 4, 'mapcgpdm': 5}
 
 
 mp_color = {
@@ -35,7 +35,7 @@ fig, ax = plt.subplots()
 for label, d in df.groupby('mp_type'):
     if label in ['mapgpdm', 'mapcgpdm']:
         continue
-    # if label not in ['tmp']:
+    # if label not in ['dmp']:
     #     continue
     d['X'] = d['partialMSE']
     for occ, D in d.groupby('occluded_contact'):
@@ -54,7 +54,7 @@ for label, d in df.groupby('mp_type'):
                         label=label if occ else None,
                         color=mp_color[label],
                         fmt=occ_fmt[occ])
-        # color = p[0].get_color()
+        color = p[0].get_color()
         xpred = np.linspace(0, 2)
         for occ in range(2):
             i = np.random.choice(range(2000))
@@ -64,14 +64,15 @@ for label, d in df.groupby('mp_type'):
             c = trace['c'].mean(axis=0)[mp_type_to_idx[label], occ]
             ypred = c / (1 + np.exp(-(a + b * xpred)))
             fmt = '--' if occ == 0 else '-'
-            ax.plot(xpred, ypred, alpha=1, color=mp_color[label], linestyle=fmt)
+            # ax.plot(xpred, ypred, alpha=1, color=mp_color[label], linestyle=fmt)
+            ax.plot(xpred, ypred, alpha=1, color=color, linestyle=fmt)
 plt.legend(loc='upper right')
 plt.ylim(0, 0.65)
 plt.xlim(0, 1.4)
 plt.title(f'Effect of Contact on Perception')
 plt.ylabel('Confusion Rate')
 plt.xlabel('MSE')
-plt.savefig('../SFBTRR135experiments/natMPs/talks/SAP2020_presentation/media/segmentation.svg')
+# plt.savefig('../SFBTRR135experiments/natMPs/talks/SAP2020_presentation/media/segmentation.svg')
 plt.show()
 
 # TODO: compute posterior of ypred[no occ] > ypred[occ] in xrange of data.
