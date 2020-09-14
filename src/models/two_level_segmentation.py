306226ea51e6ac0d1f708a29358c18cbca572d9c
Benjamin Knopp <knoppbe@staff.uni-marburg.de>
2020-09-14 12:23:22 +0200

    Ran segmentation analysis.

--- src/models/two_level_segmentation.py
+++ src/models/two_level_segmentation.py
@@ -4,17 +4,19 @@ import pickle
 import pymc3 as pm
 
 
-df, pmps, participants, mp_types, participant_to_idx, mp_type_to_idx, pmp_to_idx = load_data('data/processed/processed_data_online.json')
+df, pmps, participants, mp_types, participant_to_idx, mp_type_to_idx, pmp_to_idx = load_data('data/processed/processed_online_data.json')
 
 df['occ'] = df['occluded_contact'].astype(int)
 
 with pm.Model() as multilevel:
-    a_barbar = pm.Normal('a_barbar', 0, 1.5)
-    sigma_abar = pm.Exponential('sigma_abar', 1)
-    a_bar = pm.Normal('a_bar', a_barbar, sigma_abar, shape=(len(mp_types), 2))
-    b_barbar = pm.Normal('b_barbar', 0, 1.5)
-    sigma_bbar = pm.Exponential('sigma_bbar', 1)
-    b_bar = pm.Normal('b_bar', b_barbar, sigma_bbar, shape=(len(mp_types), 2))
+    # a_barbar = pm.Normal('a_barbar', 0, 1.5)
+    # sigma_abar = pm.Exponential('sigma_abar', 1)
+    # a_bar = pm.Normal('a_bar', a_barbar, sigma_abar, shape=(len(mp_types), 2))
+    a_bar = pm.Normal('a_bar', 0, 1.5, shape=(len(mp_types), 2))
+    # b_barbar = pm.Normal('b_barbar', 0, 1.5)
+    # sigma_bbar = pm.Exponential('sigma_bbar', 1)
+    # b_bar = pm.Normal('b_bar', b_barbar, sigma_bbar, shape=(len(mp_types), 2))
+    b_bar = pm.Normal('b_bar', 0, 1.5, shape=(len(mp_types), 2))
     sigma_b = pm.Exponential('sigma_b', 1, shape=(len(mp_types), 2))
     # c_bar = pm.Normal('c_bar', 0, 1.5)
     # sigma_c = pm.Exponential('sigma_c', 1)
@@ -29,7 +31,7 @@ with pm.Model() as multilevel:
     p = c[df.id_mp_type, df.occ] / (1 + np.exp(-(a_bar[df.id_mp_type, df.occ] + sigma_a[df.id_mp_type, df.occ] * a[df.id_participant, df.occ] + (b_bar[df.id_mp_type, df.occ] + sigma_b[df.id_mp_type, df.occ] * b[df.id_participant, df.occ]) * x)))
     # p = 0.5 / (1 + np.exp(-(a_bar + sigma_a* a[df.id_participant] + sigma_c*c[df.id_mp_type] + (b_bar + sigma_b * b[df.id_mp_type]) * x)))
     s = pm.Bernoulli('s', p=p, observed=df['result'])
-    trace_multilevel = pm.sample(1000, tune=1000, init='adapt_diag')
+    trace_multilevel = pm.sample(2000, tune=1000, init='adapt_diag')
 
 with open("data/processed/twolevelsegmentation_trace.pkl", "wb") as fh:
     pickle.dump((multilevel, trace_multilevel), fh)
